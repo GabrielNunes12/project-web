af072f80a24cec9ffcf18b9f22e7266776041d4a
Gabriel <prismaticnetwork12@gmail.com>
2022-11-02 19:49:34 -0300

    fix: changing to meaven instead of gradle

--- WebApp1/src/main/java/com/training/Project/WebApp1/WebApp1Application.java
+++ WebApp1/src/main/java/com/training/Project/WebApp1/WebApp1Application.java
@@ -1,13 +1,13 @@
-package com.training.Project.WebApp;
+package com.training.Project.WebApp1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WebAppApplication {
+public class WebApp1Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(WebAppApplication.class, args);
+		SpringApplication.run(WebApp1Application.class, args);
 	}
 
 }
--- WebApp1/src/test/java/com/training/Project/WebApp1/WebApp1ApplicationTests.java
+++ WebApp1/src/test/java/com/training/Project/WebApp1/WebApp1ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.training.Project.WebApp;
+package com.training.Project.WebApp1;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class WebAppApplicationTests {
+class WebApp1ApplicationTests {
 
 	@Test
 	void contextLoads() {
